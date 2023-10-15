d056fa22d09a182dbce470d70d2bb1135cc2e381
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-10-15 22:58:09 +0800

    layout format done

--- src/components/App.js
+++ src/components/App.js
@@ -6,31 +6,32 @@ function App() {
   const [output, setOutput] = useState("");
 
   return (
-    <div className="calculator">
-      <div className="output">
-        <Output output={output} />
+    <div className="container">
+      <div className="calculator">
+        <div className="output">
+          <Output output={output} />
+        </div>
+        <div className="keypad">
+          <Button>Clear</Button>
+          <Button>+-</Button>
+          <Button>&divide;</Button>
+          <Button setOutput={setOutput}>7</Button>
+          <Button setOutput={setOutput}>8</Button>
+          <Button setOutput={setOutput}>9</Button>
+          <Button>&times;</Button>
+          <Button setOutput={setOutput}>4</Button>
+          <Button setOutput={setOutput}>5</Button>
+          <Button setOutput={setOutput}>6</Button>
+          <Button>-</Button>
+          <Button setOutput={setOutput}>1</Button>
+          <Button setOutput={setOutput}>2</Button>
+          <Button setOutput={setOutput}>3</Button>
+          <Button>+</Button>
+          <Button setOutput={setOutput}>0</Button>
+          <Button setOutput={setOutput}>.</Button>
+          <Button>=</Button>
+        </div>
       </div>
-      <div className="keypad">
-        <Button value="clear">Clear</Button>
-        <Button>+-</Button>
-        <Button>&divide;</Button>
-        <Button setOutput={setOutput}>7</Button>
-        <Button setOutput={setOutput}>8</Button>
-        <Button setOutput={setOutput}>9</Button>
-        <Button>&times;</Button>
-        <Button setOutput={setOutput}>4</Button>
-        <Button setOutput={setOutput}>5</Button>
-        <Button setOutput={setOutput}>6</Button>
-        <Button>-</Button>
-        <Button setOutput={setOutput}>1</Button>
-        <Button setOutput={setOutput}>2</Button>
-        <Button setOutput={setOutput}>3</Button>
-        <Button>+</Button>
-        <Button setOutput={setOutput}>0</Button>
-        <Button>.</Button>
-        <Button>=</Button>
-      </div>
-      <img src="calculator.PNG"></img>
     </div>
   );
 }
